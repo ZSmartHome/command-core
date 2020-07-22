f5546918d7de675a80485c82329bb2c522909625
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-07-22 23:23:56 +0300

    update library

--- src/errors/web-error.ts
+++
@@ -1,5 +0,0 @@
-export class WebError extends Error {
-  constructor (public code: number, message: string) {
-    super(message);
-  }
-}
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
 export {execute as tv} from './commands/tv';
 export {execute as light} from './commands/light';
+export {ExecutionError} from './errors/execution-error';
+export {shell, split} from './util';
