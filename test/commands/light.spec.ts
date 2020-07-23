dddf8cdf626d3478287f99bedc0a9c1a16c0dce5
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-07-23 12:05:31 +0300

    rollback to jest

--- test/commands/light.spec.ts
+++ test/commands/light.spec.ts
@@ -1,12 +1,8 @@
 import * as assert from 'assert';
-import {suite, test} from 'mocha-typescript';
 import {Option} from '../../src/commands/light';
 
-@suite('Check exported values')
-export default class {
-  @test
-  public 'should export options'() {
-    assert(Option)
-  }
-
-}
+describe('Check exported values', () => {
+  it('should export options', () => {
+    assert.ok(Option);
+  })
+})
--- test/split.spec.ts
+++ test/split.spec.ts
@@ -1,5 +1,4 @@
 import * as assert from 'assert';
-import {suite, test} from 'mocha-typescript';
 import {split} from '../src';
 
 const NUMBERS = [`one`, `two`, `three`, `four`, `five`, `six`, `seven`, `eight`, `nine`, `ten`];
@@ -13,38 +12,31 @@ const assertSplit = (source: any[], ...rows: number[]) => {
   }
 };
 
-@suite('Verify split util')
-export default class {
-  @test
-  public 'should be single row'() {
+describe(`Verify split util`, () => {
+  it('should be single row', () => {
     assertSplit(NUMBERS, 10);
-  }
+  });
 
-  @test
-  public 'should be two rows'() {
+  it(`should be two rows`, () => {
     assertSplit(NUMBERS, 5, 5);
-  }
+  });
 
-  @test
-  public 'should split on two rows unequally'() {
+  it(`should split on two rows unequally`, () => {
     assertSplit(NUMBERS.slice(0, 5), 2, 3);
-  }
+  });
 
-  @test
-  public 'should split on 3 rows'() {
+  it(`should split on 3 rows`, () => {
     assertSplit(NUMBERS.slice(0, 5), 2, 2, 1);
-  }
+  });
 
-  @test
-  public 'should be three rows'() {
+  it(`should be three rows`, () => {
     assertSplit(NUMBERS, 3, 3, 3, 1);
-  }
+  });
 
-  @test
-  public 'should overflow last row'() {
+  it(`should overflow last row`, () => {
     const result = split(NUMBERS, 2, 1);
     assert.strictEqual(result.length, 2);
     assert.strictEqual(result[0].length, 2);
     assert.strictEqual(result[1].length, 8);
-  }
-}
+  });
+});
