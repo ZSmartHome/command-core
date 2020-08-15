ecf2e8bad07cedf156af7ad649160c6b0c426123
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-08-16 01:21:44 +0300

    fix lint

--- test/commands/light.spec.ts
+++ test/commands/light.spec.ts
@@ -1,19 +1,19 @@
 import {light, lightCommands} from '../../src';
 
-describe('Check exported values', () => {
-  it('should throw on unknown commands', async () => {
+describe(`Check exported values`, () => {
+  it(`should throw on unknown commands`, async () => {
     // BC! You can't them parallel it's RaceCondition on port
-    await expect(light('test')).rejects.toThrow(`Unsupported command: test`);
-    await expect(light('  test  ')).rejects.toThrow(`Unsupported command: test`);
-    await expect(light('unknown')).rejects.toThrow(`Unsupported command: unknown`);
+    await expect(light(`test`)).rejects.toThrow(`Unsupported command: test`);
+    await expect(light(`  test  `)).rejects.toThrow(`Unsupported command: test`);
+    await expect(light(`unknown`)).rejects.toThrow(`Unsupported command: unknown`);
   })
-  it('should throw on empty command', async () => {
+  it(`should throw on empty command`, async () => {
     // BC! You can't them parallel it's RaceCondition on port
-    await expect(light('')).rejects.toThrow(`What should I do with Light?`);
-    await expect(light('    ')).rejects.toThrow(`What should I do with Light?`);
-    await expect(light('    ')).rejects.toThrow(`What should I do with Light?`);
+    await expect(light(``)).rejects.toThrow(`What should I do with Light?`);
+    await expect(light(`    `)).rejects.toThrow(`What should I do with Light?`);
+    await expect(light(`    `)).rejects.toThrow(`What should I do with Light?`);
   })
-  it('should check number of commands', async () => {
+  it(`should check number of commands`, async () => {
     expect(Object.keys(lightCommands).length).toBe(9);
   })
 })
