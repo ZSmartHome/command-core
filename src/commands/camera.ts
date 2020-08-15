2f79fd43f67e0d39cb61568ffef18305cd04e0e3
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-08-16 01:55:44 +0300

    add camera executor

---
+++ src/commands/camera.ts
@@ -0,0 +1,29 @@
+import Executor from '../executor/executor';
+import {Stream} from 'stream';
+import {Option} from '../executor/option';
+import {get} from "http";
+import {ExecutionError} from '..';
+
+export class CameraExecutor extends Executor<Option, Stream> {
+  constructor(private url: string) {
+    super(`Camera`, {
+      default: {label: `One shot 📷`}
+    });
+  }
+
+  protected async execute(option: Option): Promise<Stream> {
+    return new Promise((resolve, reject) => {
+      get(this.url, (response) => {
+        const statusCode = response.statusCode;
+        if (statusCode !== 200) {
+          reject(new ExecutionError(`Failed to fetch media. Response code: ${statusCode}`));
+        }
+        // @ts-ignore
+        response.path = `camera.jpg`; // NB! Due to issue in library check set this always
+        resolve(response);
+      })
+    })
+  }
+}
+
+export const getExecutor = (url) => new CameraExecutor(url);
