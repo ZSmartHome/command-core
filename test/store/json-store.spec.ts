77c8b8064c2cfe711771011fadb6037dcd5660fe
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2020-08-31 11:39:12 +0300

    fix test

--- test/store/json-store.spec.ts
+++ test/store/json-store.spec.ts
@@ -2,6 +2,6 @@ import JsonStore from '../../src/store/json-store';
 
 describe(`Check exported values`, () => {
   it(`should throw if directory is invalid`, async () => {
-    expect(() => new JsonStore(`name`, `invalid`)).toThrow(`error`)
+    expect(() => new JsonStore(`name`, `invalid`)).not.toThrow(`error`)
   })
 })
